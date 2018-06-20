6b9ea5e059de708ab1e802ef7ffeca3a9554769b
DanielWysowski <32766200+DanielWysowski@users.noreply.github.com>
2018-06-20 22:23:19 +0200

    Add files via upload

---
+++ Labolatorium_7/src/Class_1.java
@@ -0,0 +1,14 @@
+public class Class_1 {
+}
+
+import jade.core.Agent;
+
+public class Class_1 extends Agent {
+
+    protected void setup() {
+        System.out.println("startuje!");
+    }
+    protected void takeDown() {
+        System.out.println("zaraz sie usune!");
+    }
+}
